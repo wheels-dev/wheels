cddb691c008e06956554964de34325d19d828797
Peter Amiri <peter@alurium.com>
2025-06-30 07:07:12 -0700

    use base url

--- tools/docker/github/core-tests.sh
+++ tools/docker/github/core-tests.sh
@@ -54,8 +54,8 @@ case $1 in
     ;;
 esac
 
-# test_url="http://localhost:${port}/wheels/testbox?db=${dbengine}&format=json&only=failure,error"
-test_url="http://localhost:${port}/"
+test_url="http://localhost:${port}/wheels/testbox?db=${dbengine}&format=json&only=failure,error"
+# test_url="http://localhost:${port}/"
 result_file="/tmp/${cfengine}-${db}-result.txt"
 
 echo "\nRUNNING SUITE (${cfengine}/${dbengine}):\n"
