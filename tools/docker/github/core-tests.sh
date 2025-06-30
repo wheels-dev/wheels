4c77377a6d69a47abd2e5457a24eb5779b080530
Peter Amiri <peter@alurium.com>
2025-06-30 05:51:11 -0700

    extract port

--- tools/docker/github/core-tests.sh
+++ tools/docker/github/core-tests.sh
@@ -12,11 +12,49 @@ dbengine=${2}
 
 ./functions.sh
 
+case $1 in
+  lucee5)
+    port="60005"
+    ;;
+  lucee6)
+    port="60006"
+    ;;
+  lucee7)
+    port="60007"
+    ;;
+  adobe2018)
+    port="62018"
+    ;;
+  adobe2021)
+    port="62021"
+    ;;
+  adobe2023)
+    port="62023"
+    ;;
+  adobe2025)
+    port="62025"
+    ;;
+  mysql56)
+    port="3306"
+    ;;
+  sqlserver)
+    port="1433"
+    ;;
+  postgres)
+    port="5432"
+    ;;
+  h2)
+    port="9092"
+    ;;
+  oracle)
+    port="1521"
+    ;;
+  *)
+    port="unknown"
+    ;;
+esac
 
-port="$(get_port ${cfengine})"
-db="$(get_db ${dbengine})"
-
-test_url="http://127.0.0.1:${port}/wheels/testbox?db=${db}&format=json&only=failure,error"
+test_url="http://127.0.0.1:${port}/wheels/testbox?db=${dbengine}&format=json&only=failure,error"
 result_file="/tmp/${cfengine}-${db}-result.txt"
 
 echo "\nRUNNING SUITE (${cfengine}/${dbengine}):\n"
