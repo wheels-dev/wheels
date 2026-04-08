73f45679a4618fe601611c4ede31ca7cb9fa6ab6
zainforbjs <zain.shared@gmail.com>
2026-04-08 17:25:49 +0500

    Dynamic port mapping for playwright
    Updated the playwright setup to set the port for the base_URL dynamically.

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,17 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import fs from 'fs';
+dotenv.config();
+
+let baseURL = process.env.BASE_URL;
+
+if (!baseURL) {
+  const serverConfig = JSON.parse(fs.readFileSync('./server.json', 'utf-8'));
+  const port = serverConfig?.web?.http?.port || 8080;
+  baseURL = `http://127.0.0.1:${port}`;
+}
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -26,7 +34,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://127.0.0.1:8082',
+    baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
